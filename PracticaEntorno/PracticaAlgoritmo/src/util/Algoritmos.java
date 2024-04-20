8822d1e76af583047af46dcca86fcbea1c99cfbc
tingwu420 <162586562+tingwu420@users.noreply.github.com>
2024-04-20 23:26:25 +0200

    Metodo EsPrimo

--- PracticaEntorno/PracticaAlgoritmo/src/util/Algoritmos.java
+++ PracticaEntorno/PracticaAlgoritmo/src/util/Algoritmos.java
@@ -17,4 +17,25 @@ public abstract class Algoritmos {
             return numero * factorial(numero - 1);
         }
     }
+	
+	
+	public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        if (numero <= 3) {
+            return true;
+        }
+        if (numero % 2 == 0 || numero % 3 == 0) {
+            return false;
+        }
+        int divisor = 5;
+        while (divisor * divisor <= numero) {
+            if (numero % divisor == 0 || numero % (divisor + 2) == 0) {
+                return false;
+            }
+            divisor += 6;
+        }
+        return true;
+    }
 }
